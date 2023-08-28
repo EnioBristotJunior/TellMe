9526a346811c82098ae290682b7d33f593cda0c7
Enio Bristot Junior <eniobristotjunior@gmail.com>
2023-08-28 12:23:26 -0300

    Estilização da tela de criar área

--- src/screens/Areas/NewArea/index.js
+++ src/screens/Areas/NewArea/index.js
@@ -1,31 +1,54 @@
+//React
 import React, { useEffect } from "react";
-import { View, Dimensions } from "react-native";
 
-import { Container } from "./styles";
+//React Native
+import { View, Dimensions, TouchableOpacity } from "react-native";
 
+//Componentes
+import {
+  AlertView,
+  Bottom,
+  Confirm,
+  Container,
+  Form,
+  Header,
+  Input,
+  Main,
+  SelectImage,
+  TextAlert,
+  TextButton,
+  TextImage,
+  Title,
+} from "./styles";
+
+//SVG
 import BgSvg from "../../../imgs/Areas/backArea-g9.svg";
 
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+
+//Dimensão da tela
 const { width, height } = Dimensions.get("screen");
 
 export function NewArea({ navigation }) {
+  //Remoção do bottom navigator
   useEffect(() => {
     navigation.getParent().setOptions({ tabBarStyle: { display: "none" } });
     return () => {
-      navigation
-        .getParent()
-        .setOptions({
-          tabBarStyle: {
-            display: "flex",
-            backgroundColor: "#FF7F00",
-            height: 70,
-            position: "absolute",
-            bottom: 30,
-            right: 40,
-            borderRadius: 30,
-            left: 40,
-            borderTopWidth: 0,
-          },
-        });
+      navigation.getParent().setOptions({
+        tabBarStyle: {
+          display: "flex",
+          backgroundColor: "#FF7F00",
+          height: 70,
+          position: "absolute",
+          bottom: 30,
+          right: 40,
+          borderRadius: 30,
+          left: 40,
+          borderTopWidth: 0,
+        },
+      });
     };
   }, []);
 
@@ -36,6 +59,54 @@ export function NewArea({ navigation }) {
         height={height}
         style={{ position: "absolute", top: 0, left: 0, zIndex: -1 }}
       />
+      <Main>
+        <Header>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.getParent().setOptions({
+                tabBarStyle: {
+                  display: "flex",
+                  backgroundColor: "#FF7F00",
+                  height: 70,
+                  position: "absolute",
+                  bottom: 30,
+                  right: 40,
+                  borderRadius: 30,
+                  left: 40,
+                  borderTopWidth: 0,
+                },
+              });
+              navigation.goBack();
+            }}
+          >
+            <AntDesign name="arrowleft" size={35} color={"#FF7F00"} />
+          </TouchableOpacity>
+
+          <Title>Criar nova área</Title>
+        </Header>
+        <Form>
+          <SelectImage>
+            <FontAwesome5 name="camera" size={50} color={"#091837"} />
+            <TextImage>Adicionar imagem</TextImage>
+          </SelectImage>
+          <View style={{ gap: 8 }}>
+            <Input
+              placeholder="Título da área"
+              placeholderTextColor={"#d9d9d9"}
+              cursorColor={"#ff7f00"}
+            />
+            <AlertView>
+              <Ionicons name="alert-circle-outline" size={24} color="#FF7F00" />
+              <TextAlert>Máximo de 14 caracteres</TextAlert>
+            </AlertView>
+          </View>
+        </Form>
+      </Main>
+      <Bottom>
+        <Confirm>
+          <TextButton>Criar Área</TextButton>
+        </Confirm>
+      </Bottom>
     </Container>
   );
 }
--- src/screens/Areas/NewArea/styles.js
+++ src/screens/Areas/NewArea/styles.js
@@ -4,4 +4,86 @@ export const Container = styled.View`
   flex: 1;
   z-index: 1;
   background-color: ${(props) => props.theme.background};
+  padding: 25px;
+  justify-content: space-between;
+`;
+
+export const Main = styled.View`
+  gap: 25px;
+  margin-top: 40px;
+`;
+
+export const Header = styled.View`
+  gap: 10px;
+`;
+
+export const Form = styled.View`
+  gap: 15px;
+`;
+
+export const Title = styled.Text`
+  font-size: 32px;
+  font-family: "ComicNeue_700Bold";
+  color: ${(props) => props.theme.color};
+`;
+
+//Forms
+
+export const SelectImage = styled.TouchableOpacity`
+  width: 100%;
+  height: 200px;
+  background-color: ${(props) => props.theme.secondBackground};
+  border-radius: 12px;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const TextImage = styled.Text`
+  font-size: 16px;
+  font-family: "Comfortaa_700Bold";
+  color: ${(props) => props.theme.background};
+`;
+
+export const Input = styled.TextInput`
+  background-color: transparent;
+  width: 100%;
+  height: 65px;
+  border-radius: 10px;
+  border: 1px solid ${(props) => props.theme.secondBackground};
+  padding-left: 25px;
+  color: ${(props) => props.theme.color};
+  font-family: "Comfortaa_500Medium";
+`;
+
+export const AlertView = styled.View`
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+`;
+
+export const TextAlert = styled.Text`
+  font-size: 13px;
+  font-family: "Comfortaa_700Bold";
+  color: ${(props) => props.theme.secondBackground};
+`;
+
+//Botão de confirmar
+
+export const Bottom = styled.View``;
+
+export const Confirm = styled.TouchableOpacity`
+  width: 100%;
+  height: 65px;
+  background-color: ${(props) => props.theme.secondBackground};
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  margin-bottom: 12px;
+`;
+
+export const TextButton = styled.Text`
+  font-size: 20px;
+  font-family: "ComicNeue_700Bold";
+  color: ${(props) => props.theme.background};
 `;
